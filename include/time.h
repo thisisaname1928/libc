ca82433b01d4951966aea502d6f8d14d8275f5d2
ngqt <nguyenquoctrungap47@gmail.com>
2026-06-01 22:30:16 +0700

    feat: add struct timespec

--- include/time.h
+++ include/time.h
@@ -9,15 +9,20 @@ typedef unsigned long long clock_t;
 #define CLOCKS_PER_SEC 3000000000ULL
 
 struct tm {
-    int tm_sec;
-    int tm_min;
-    int tm_hour;
-    int tm_mday;
-    int tm_mon;
-    int tm_year;
-    int tm_wday;
-    int tm_yday;
-    int tm_isdst;
+  int tm_sec;
+  int tm_min;
+  int tm_hour;
+  int tm_mday;
+  int tm_mon;
+  int tm_year;
+  int tm_wday;
+  int tm_yday;
+  int tm_isdst;
+};
+
+struct timespec {
+  time_t tv_sec;
+  long tv_nsec;
 };
 
 time_t time(time_t *out);
